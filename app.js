24c2676d9c883c805b679069313c7145586b61f6
alaaddin <alaaddinsagun60@gmail.com>
2024-03-31 01:17:14 +0100

    Finish basic styles and layouts bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const path = require('path')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
+const ejsMate = require('ejs-mate')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname='views'))
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/campblog').then(() => {
     console.log("Connecting to MongoDB failed!!!")
 })
 
+app.engine('ejs',ejsMate)
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
